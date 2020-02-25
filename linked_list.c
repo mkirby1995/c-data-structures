f045165414313f7fd009eef5b7f9425db59b73e3
Matt.Kirby <mkirby3@angelo.edu>
2020-02-24 18:53:34 -0600

    finish linked list

--- linked_list.c
+++ linked_list.c
@@ -43,53 +43,86 @@ void push_to_tail(Linked_List *list, int value)
     list->length ++;
 };
 
-// Insert at index
-void insert(Linked_List *list, int value, int index)
-{
-    int i = 1;
-    Node *current = list->head->next;
-    Node *previous = list->head;
-    Node *new_node = create_node(value);
-    while (i < list->length && i != index) {
-        previous = previous->next;
-        current = current->next;
-        i++;
-    };
-    //if (i == list.length)
-    new_node->next = current;
-    previous->next = new_node;
-    list->length++;
-};
-
 
 // Push to head
 void push_to_head(Linked_List *list, int value)
 {
-    insert(list, value, 0);
+    Node *current = list->head;
+    Node *new_node = create_node(value);
+    new_node->next = current;
+    list->head = new_node;
+    list->length++;
 };
 
 
-// Remove Node
+// Insert at index
+void insert(Linked_List *list, int value, int index)
+{
+    if (index == 0) {
+        push_to_head(list, value);
+        list->length++;
+    } else if (index == list->length) {
+        push_to_tail(list, value);
+        list->length++;
+    } else if (index > list->length) {
+        printf("Index too large.\n");
+    } else {
+        int i = 1;
+        Node *current = list->head->next;
+        Node *previous = list->head;
+        Node *new_node = create_node(value);
+        while (i < list->length && i != index) {
+            previous = previous->next;
+            current = current->next;
+            i++;
+        };
+        new_node->next = current;
+        previous->next = new_node;
+        list->length++;
+    };
+};
+
+
+// Remove Node at index
 void remove_node(Linked_List *list, int index)
 {
-    int i = 0;
-    Node *current = list->head->next;
-    Node *previous = list->head;
-    while (i < list->length && i != index) {
-        previous = previous->next;
-        current = current->next;
-        i++;
+    if (index > list->length) {
+        printf("No node to remove\n");
+    } else if (index == 0) {
+        Node *current = list->head->next;
+        Node *previous = list->head;
+        list->head = current;
+        free(previous);
+        list->length--;
+    } else {
+        int i = 0;
+        Node *current = list->head->next;
+        Node *previous = list->head;
+        while (i < list->length && i < index - 1) {
+            previous = previous->next;
+            current = current->next;
+            i++;
+        };
+        previous->next = current->next;
+        free(current);
+        list->length--;
     };
-    previous->next = current->next;
-    list->length++;
 };
 
 
 // Destroy list
 void destroy_list(Linked_List *list)
 {
-    Node *nodes = list->head->next;
-    free(nodes);
+    int i = 0;
+    Node *current = list->head->next;
+    Node *previous = list->head;
+    for (i = 0; i < list->length; i++) {
+        previous = current;
+        current = current->next;
+        free(previous);
+        list->length--;
+    };
+    free(list->head);
 }
 
 
@@ -110,11 +143,14 @@ int main()
     Linked_List ll = *create_linked_list(8);
     push_to_tail(&ll, 9);
     push_to_tail(&ll, 10);
-    //push_to_tail(&ll, 3);
-    //push_to_tail(&ll, 4);
+    push_to_head(&ll, 15);
+    push_to_tail(&ll, 3);
+    push_to_tail(&ll, 4);
     insert(&ll, 20, 2);
     remove_node(&ll, 2);
-
+    remove_node(&ll, 0);
+    remove_node(&ll, 2);
+    remove_node(&ll, 3);
     traversal(&ll);
     destroy_list(&ll);
     return 0;
